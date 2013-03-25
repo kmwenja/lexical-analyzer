6bc030d47a9d5626d8870dbd3ff21f229bf7f351
caninemwenja <mwenja07@gmail.com>
2013-03-25 16:58:30 +0300

    added codes to the lookup file and supporting code

--- lexical_analyzer.py
+++ lexical_analyzer.py
@@ -10,31 +10,40 @@ import source_compartment
 import delimiter_compartment
 import token_compartment
 
+
 class LexicalAnalyzer(object):
-    _source_compartment = None
-    _delimiter_compartment = None
-    _token_compartment = None
+    '''the lexical analyzer proper'''
     
     def __init__(self, source_code, lookup_table):
+        '''initialize the analyzer with its components'''
+        
+        # initialize the source compartment with the source code
         self._source_compartment = source_compartment.SourceCompartment(source_code)
+        
+        # initialize the delimiter compartment with the source compartment
         self._delimiter_compartment = delimiter_compartment.DelimiterCompartment(self._source_compartment)
+        
+        # initialize the token compartment with the delimiter and the lookup table
         self._token_compartment = token_compartment.TokenCompartment(self._delimiter_compartment, lookup_table)
     
     def next_token(self):
+        '''return the next token gotten from the token compartment'''
         return self._token_compartment.nextToken()
 
-def get_lookup_table(filename,add_space=True):
+def get_lookup_table(filename, elements=2, add_space=True):
+    '''retrieve the lookup table from a file
+    
+    the lookup file should have the regex, token type 
+    '''
     lookup_table = []
     
     f = open(filename)
     for i in f:
-        # take the first two elements in the line
-        splits = i.split(" ", 2) # elements must be separated by a space
+        splits = i.split(" ", elements) # elements must be separated by a space
         
-        regex = splits[0]
-        token_type = splits[1][:-1] # remove the new line from the last element
+        splits[-1] = splits[-1][:-1] # remove the new line from the last element
         
-        token = (regex, token_type)
+        token = tuple(splits)
         lookup_table.append(token)
     
     if add_space:
@@ -56,7 +65,7 @@ def main():
         f.close()
         
         # get the lookup table
-        lookup_table = get_lookup_table(lookup_file)
+        lookup_table = get_lookup_table(lookup_file, elements=3)
         
         # initialize the analyzer
         la = LexicalAnalyzer(source_code, lookup_table)
